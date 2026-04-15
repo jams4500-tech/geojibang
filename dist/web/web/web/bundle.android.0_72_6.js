2ffdce41f8aa98b436bbe1adab4bd927a78eefd1
선민영 <seonmin-yeong@seonmin-yeong-ui-MacBookPro.local>
2026-04-15 14:30:40 +0900

    탭 순서 변경 및 토스포인트 5P 교환 위치 조정
    - 탭 순서: 내 거지 → 거슐랭 → 피드 → 내 지출 → 거지 랭킹
    - 기본 active 탭을 tab-geoji로 변경
    - 내 거지 탭에서 토스포인트 5P 교환 버튼을 구걸 멘트 뽑기 위로 이동

--- dist/web/web/web/bundle.android.0_72_6.js
+++ dist/web/web/web/bundle.android.0_72_6.js
@@ -9,7 +9,7 @@ var global = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019d475a-d7ba-7b4c-a37a-e650a7e3c360",
+    deploymentId: "019d8402-a83f-7b86-be5a-ee74c24f1220",
     brandDisplayName: "\uAC70\uC9C0 \uCC4C\uB9B0\uC9C0",
     brandPrimaryColor: "#e8527a",
     brandIcon: "https://static.toss.im/appsintoss/30705/1e023176-068e-42b8-811c-91bb763ab1fd.png",
@@ -132,7 +132,7 @@ if (global.__MICRO_FRONTEND__ == null) {
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019d475a-d7ba-7b4c-a37a-e650a7e3c360",
+    deploymentId: "019d8402-a83f-7b86-be5a-ee74c24f1220",
     brandDisplayName: "\uAC70\uC9C0 \uCC4C\uB9B0\uC9C0",
     brandPrimaryColor: "#e8527a",
     brandIcon: "https://static.toss.im/appsintoss/30705/1e023176-068e-42b8-811c-91bb763ab1fd.png",
