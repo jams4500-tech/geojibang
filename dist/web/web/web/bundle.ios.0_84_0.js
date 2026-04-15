255ecedbcccb86732581b36c3a7e2c0ce2fbe4e2
선민영 <seonmin-yeong@seonmin-yeong-ui-MacBookPro.local>
2026-04-15 15:07:39 +0900

    showTab('geoji') 초기 호출 위치를 스크립트 끝으로 이동
    GEOJI_TAP_PER_AD, MAP_CATEGORIES 등 const 선언보다 앞에서 호출돼
    TDZ 에러로 init이 중단되던 문제 수정

--- dist/web/web/web/bundle.ios.0_84_0.js
+++ dist/web/web/web/bundle.ios.0_84_0.js
@@ -9,7 +9,7 @@ var global = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019d8fb1-6008-7b85-a9d8-cd21acf04839",
+    deploymentId: "019d8fbc-ebef-7b5d-a8ef-0377ff39768e",
     brandDisplayName: "\uAC70\uC9C0 \uCC4C\uB9B0\uC9C0",
     brandPrimaryColor: "#e8527a",
     brandIcon: "https://static.toss.im/appsintoss/30705/1e023176-068e-42b8-811c-91bb763ab1fd.png",
@@ -132,7 +132,7 @@ if (global.__MICRO_FRONTEND__ == null) {
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019d8fb1-6008-7b85-a9d8-cd21acf04839",
+    deploymentId: "019d8fbc-ebef-7b5d-a8ef-0377ff39768e",
     brandDisplayName: "\uAC70\uC9C0 \uCC4C\uB9B0\uC9C0",
     brandPrimaryColor: "#e8527a",
     brandIcon: "https://static.toss.im/appsintoss/30705/1e023176-068e-42b8-811c-91bb763ab1fd.png",
