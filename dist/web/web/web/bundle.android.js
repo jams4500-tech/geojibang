873b615c756568bb5c776d85ed3a0788719ccd42
선민영 <seonmin-yeong@seonmin-yeong-ui-MacBookPro.local>
2026-04-15 14:49:56 +0900

    showTab 탭 인덱스 배열을 새 순서에 맞게 수정
    탭 순서 변경 후 showTab의 active 토글 배열이 옛 순서로 남아있어
    탭 버튼과 컨텐츠의 active 상태가 꼬이던 문제 수정

--- dist/web/web/web/bundle.android.js
+++ dist/web/web/web/bundle.android.js
@@ -9,7 +9,7 @@ var global = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019d8402-a83f-7b86-be5a-ee74c24f1220",
+    deploymentId: "019d8fa5-f317-728e-979b-4b62a9ec55d5",
     brandDisplayName: "\uAC70\uC9C0 \uCC4C\uB9B0\uC9C0",
     brandPrimaryColor: "#e8527a",
     brandIcon: "https://static.toss.im/appsintoss/30705/1e023176-068e-42b8-811c-91bb763ab1fd.png",
@@ -132,7 +132,7 @@ if (global.__MICRO_FRONTEND__ == null) {
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019d8402-a83f-7b86-be5a-ee74c24f1220",
+    deploymentId: "019d8fa5-f317-728e-979b-4b62a9ec55d5",
     brandDisplayName: "\uAC70\uC9C0 \uCC4C\uB9B0\uC9C0",
     brandPrimaryColor: "#e8527a",
     brandIcon: "https://static.toss.im/appsintoss/30705/1e023176-068e-42b8-811c-91bb763ab1fd.png",
